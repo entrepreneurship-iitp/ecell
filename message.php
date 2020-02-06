7d977b82114d4a255b11bd7481f771be19efb6f7
vsaisujeeth <vsaisujeeth10@yahoo.com>
2020-02-06 11:46:00 +0530

    added online db

---
+++ message.php
@@ -0,0 +1,19 @@
+<?php
+    require('./config.php');
+    if(isset($_POST['name']) && !empty($_POST['email']) AND isset($_POST['subject']) && !empty($_POST['message'])){
+        
+        
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $message=$_POST['message'];
+        $subject=$_POST['subject'];
+        $sql="  INSERT INTO messages "."VALUES('$name','$email','$subject','$message')";
+        if ($mysqli->query($sql) === TRUE) {
+            echo "Message Sent!";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        
+        $mysqli->close();
+    }
+?>
