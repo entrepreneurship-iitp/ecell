7a61270879415237408eb380795298661efd56a4
vsaisujeeth <vsaisujeeth10@yahoo.com>
2020-03-05 11:05:34 +0530

    minor

--- mail_test.php
+++ mail_test.php
@@ -2,6 +2,7 @@
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
+echo "hello";
 # Instantiate the client.
 $mgClient = Mailgun::create('88e840e8b2e3be2d09c5ba2f0d9b2f5a-c322068c-00166fda');
 $domain = "sandbox50dba53faf704047af618d19163d7ec2.mailgun.org";
@@ -11,6 +12,8 @@ $params = array(
 'subject' => 'Hello',
 'text' => 'Testing some Mailgun awesomness!'
 );
+
+echo "hi";
 
 $mgClient->messages()->send($domain, $params);
 ?>
