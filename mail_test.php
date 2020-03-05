4ecb44955c3cafa45e697bef8954ac5413377b6b
vsaisujeeth <vsaisujeeth10@yahoo.com>
2020-03-05 11:21:16 +0530

    minor

--- mail_test.php
+++ mail_test.php
@@ -4,17 +4,16 @@ require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 echo "hello";
 # Instantiate the client.
-$mgClient = new Mailgun('88e840e8b2e3be2d09c5ba2f0d9b2f5a-c322068c-00166fda');
-echo "test"
-$domain = "sandbox50dba53faf704047af618d19163d7ec2.mailgun.org";
-echo "frit";
-# Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-	'from'	=> 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-	'to'	=> 'Baz <YOU@YOUR_DOMAIN_NAME>',
-	'subject' => 'Hello',
-	'text'	=> 'Testing some Mailgun awesomness!'
-));
+$mgClient = Mailgun::create('88e840e8b2e3be2d09c5ba2f0d9b2f5a-c322068c-00166fda');
+echo "created";
+$domain = "ecell-iitp.org";
+$params = array(
+'from' => 'Excited User <noreply@ecell-iitp.org>',
+'to' => 'vsaisujeeth10@gmail.com',
+'subject' => 'Hello',
+'text' => 'Testing some Mailgun awesomness!'
+);
+
 echo "hi";
 
 $mgClient->messages()->send($domain, $params);
