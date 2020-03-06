a4042de3d104fcb4117469158e63bde26bb49f51
vsaisujeeth <vsaisujeeth10@yahoo.com>
2020-03-06 10:44:52 +0530

    minor

--- mail_test.php
+++ mail_test.php
@@ -1,20 +1,24 @@
 <?php
-# Include the Autoloader (see "Libraries" for install instructions)
+
+// If you are using Composer
 require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-echo "hello";
-# Instantiate the client.
-$mgClient = Mailgun::create('88e840e8b2e3be2d09c5ba2f0d9b2f5a-c322068c-00166fda');
-echo "created";
-$domain = "ecell-iitp.org";
-$params = array(
-'from' => 'Excited User <noreply@ecell-iitp.org>',
-'to' => 'vsaisujeeth10@gmail.com',
-'subject' => 'Hello',
-'text' => 'Testing some Mailgun awesomness!'
-);
 
-echo "hi";
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
 
-$mgClient->messages()->send($domain, $params);
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "vsaisujeeth10@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = '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';
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
+
+
 ?>
